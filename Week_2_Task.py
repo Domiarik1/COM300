30d064975e6be20c45af3374f7ff922d467eeaf7
DOMINION ARIKPO <0arikd42@solent.ac.uk>
2026-01-30 15:50:54 +0000

    Week2

--- Week_2_Task.py
+++ Week_2_Task.py
@@ -1,3 +1,14 @@
-from mpl_toolkits.mplot3d.proj3d import world_transformation
+age = int(input("How old are you? "))
+if age <= 0:
+    print("You are old enough to travel.")
+    my_age = 22
+if age == my_age:
+        print("You are old enough to travel.")
 
-age = int(input("Enter your age: "))
+day = int(input(" Enter the day: "))
+month = int(input(" Enter the month: "))
+if day == 1 and month == 4:
+    print("You are a teenager.")
+elif day == 2 and month == 5:
+if age >= 22:
+    print("You are a teenager.")
