405415372e2c9a39bade6dc6293b49409ed0def6
DOMINION ARIKPO <0arikd42@solent.ac.uk>
2026-02-03 12:28:15 +0000

    Week 2else task

--- Week_2_Task.py
+++ Week_2_Task.py
@@ -1,14 +1,14 @@
 age = int(input("How old are you? "))
 if age <= 0:
-    print("You are old enough to travel.")
-    my_age = 22
+  print("You are old enough to travel.")
+    my_age = 20
 if age == my_age:
-        print("You are old enough to travel.")
-
+  print("You are old enough to travel.")
 day = int(input(" Enter the day: "))
 month = int(input(" Enter the month: "))
 if day == 1 and month == 4:
-    print("You are a teenager.")
-elif day == 2 and month == 5:
-if age >= 22:
-    print("You are a teenager.")
+  print("it is April fools day")
+if age >= 18:
+  print("You are old enough to travel.")
+else:
+  print("You are old enough to travel.")
